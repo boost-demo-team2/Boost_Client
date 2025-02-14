d5ca2631b3fc929c42ee67b0ee0207056e6fde37
eunhseong <eunhseong@gmail.com>
2025-02-14 23:38:58 +0900

    [Feat] 그룹 목록 페이지 구현

--- boost-fe/src/components/group/styled.jsx
+++ boost-fe/src/components/group/styled.jsx
@@ -134,3 +134,149 @@ export const RemoveButton = styled.span`
   margin-left: 5px;
   font-weight: bold;
 `
+
+// PublicGroupListPage 관련 스타일
+export const Container = styled.div`
+  padding: 20px;
+  max-width: 100%;
+  margin: 0 auto;
+`;
+
+export const EmptyState = styled.div`
+  text-align: center;
+  margin-top: 50px;
+`;
+
+export const Icon = styled.img`
+  width: 150px;
+  margin-bottom: 20px;
+`;
+
+export const Message = styled.p`
+  font-size: 18px;
+  color: #8d8d8d;
+  line-height: 1.5;
+`;
+
+export const SubMessage = styled.p`
+  font-size: 14px;
+  color: #b8b8b8;
+  margin-bottom: 20px;
+`;
+
+export const CreateGroupButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #282828;
+  color: #fafafa;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #282828;
+  }
+`;
+
+export const GroupList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  gap: 20px;
+  margin: 30px 160px;
+`;
+
+// GroupCard 관련 스타일
+export const Card = styled.div`
+  //width: 100%;
+  max-width: 340px;
+  background: #fafafa;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  transition: transform 0.2s ease-in-out;
+  
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fafafa;
+`;
+
+export const GroupImage = styled.img`
+  width: 90%;
+  height: 90%;
+  border-radius: 5px;
+  object-fit: cover;
+`;
+
+export const CardContent = styled.div`
+  padding: 16px;
+`;
+
+export const MetaInfo = styled.div`
+  display: flex;
+  font-size: 14px;
+  color: #282828;
+  margin-bottom: 8px;
+`;
+
+export const Bar = styled.div`
+  border-left: 1px solid #B8B8B8;
+  padding-left: 10px;
+  margin-top: 3px;
+  height: 16px;
+`;
+
+export const Date = styled.div`
+  font-weight: bold;
+  padding-right: 10px;
+`;
+
+export const Privacy = styled.div`
+  color: #8D8D8D;
+`;
+
+export const GroupTitle = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 6px;
+`;
+
+export const GroupDescription = styled.p`
+  font-size: 14px;
+  color: #282828;
+  margin-bottom: 12px;
+`;
+
+export const Stats = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const StatItem = styled.div`
+  font-size: 14px;
+  color: #B8B8B8;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 5px;
+
+  img {
+    width: 16px;
+    height: 16px;
+  }
+
+  strong {
+    font-weight: bold;
+    color: #282828;
+
+  }
+`;
--- boost-fe/src/router.jsx
+++ boost-fe/src/router.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createBrowserRouter,Navigate } from 'react-router-dom';
 import Detailpage  from './pages/Detailpage';
-import MakeGroupPage from './pages/MakeGrouppage';
-
-
+import MakeGroutppage from './pages/MakeGrouppage';
+import PublicGroupListPage from './pages/PublicGroupListPage';
+import PrivateGroupListPage from './pages/PrivateGroupListPage.jsx';
 
 const router = createBrowserRouter([
     { path : "/detail", element: <Detailpage/>},
-    { path : "/makegroup", element : <MakeGroupPage/>},
-
+    { path : "/makegroup", element: <MakeGroutppage/>},
+    { path : "/publicgroup", element: <PublicGroupListPage/>},
+    { path : "/privategroup", element: <PrivateGroupListPage />},
 ]);
 
 export default router;
